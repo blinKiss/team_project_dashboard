06a664107a577f713f859bd2fede3e3aa5ff1a44
SeulgiHwang <moka0402@gmail.net>
2023-06-13 23:58:12 +0900

    12

---
+++ src/main/java/com/human/Project230609Application.java
@@ -0,0 +1,13 @@
+package com.human;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project230609Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project230609Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/human/Project230609ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project230609ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

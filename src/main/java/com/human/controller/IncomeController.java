6bf0712329e75eb1dd3953d782c8721e02fcf312
blinKiss <blinkiss@naver.com>
2023-06-19 02:27:56 +0900

    19

---
+++ src/main/java/com/human/controller/IncomeController.java
@@ -0,0 +1,22 @@
+package com.human.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IncomeController {
+
+	@RequestMapping("/input/income")
+	public String inputIncome() {
+
+		return "input/income";
+	}
+	
+	@PostMapping
+	public String saveIncome(@RequestParam("income") int income) {
+		
+		return "redirect:/input/income";
+	}
+}
